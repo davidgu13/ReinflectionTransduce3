91d6979cb03f8c62150d5f5c8b8d45c448818610
davidgu13 <davidgu1312@gmail.com>
2022-08-07 11:41:10 +0300

    Code cleanup in the network & main files

--- stack_lstms.py
+++ stack_lstms.py
@@ -1,23 +1,28 @@
 import dynet as dy
 
+
 #############################################################
 # Stack RNNs and biRNNs
 #############################################################
 
 # from Chris Dyer and Co.'s EMNLP 2016 tutorial:
 class StackRNN(object):
-    def __init__(self, rnn, p_empty_embedding = None):
+    def __init__(self, rnn, p_empty_embedding=None):
         self.s = [(rnn.initial_state(), None)]
         self.empty = None
         if p_empty_embedding:
             self.empty = dy.parameter(p_empty_embedding)
+
     def push(self, expr, extra=None):
         self.s.append((self.s[-1][0].add_input(expr), extra))
+
     def pop(self):
-        return self.s.pop()[1] # return "extra" (i.e., whatever the caller wants or None)
+        return self.s.pop()[1]  # return "extra" (i.e., whatever the caller wants or None)
+
     def embedding(self):
         # work around since inital_state.output() is None
         return self.s[-1][0].output() if len(self.s) > 1 else self.empty
+
     def __len__(self):
         return len(self.s) - 1
 
@@ -28,20 +33,23 @@ class DeleteRNN(StackRNN):
 
 
 class StackBiRNN(object):
-    def __init__(self, frnn, brnn, p_empty_embedding = None):
+    def __init__(self, frnn, brnn, p_empty_embedding=None):
         self.frnn = frnn
         self.brnn = brnn
         self.empty = None
         if p_empty_embedding:
             self.empty = dy.parameter(p_empty_embedding)
+
     def transduce(self, embs, extras=None):
         fs = self.frnn.initial_state()
         bs = self.brnn.initial_state()
         fs_states = fs.add_inputs(embs)  # 1, 2, 3, 4
-        bs_states = reversed(bs.add_inputs(reversed(embs)))   # 1, 2, 3, 4
+        bs_states = reversed(bs.add_inputs(reversed(embs)))  # 1, 2, 3, 4
         self.s = [(fs, bs, None)] + reversed(list(zip(fs_states, bs_states, extras)))  # 0, 4, 3, 2, 1
+
     def pop(self):
-        return self.s.pop()[-1] # return "extra" (i.e., whatever the caller wants or None)
+        return self.s.pop()[-1]  # return "extra" (i.e., whatever the caller wants or None)
+
     def embedding(self):
         if len(self.s) > 1:
             fs = self.s[-1][0].output()
@@ -51,17 +59,20 @@ class StackBiRNN(object):
             # work around since inital_state.output() is None
             emb = self.empty
         return emb
+
     def __len__(self):
         return len(self.s) - 1
+
 
 class Encoder(object):
     def __init__(self, frnn, brnn):
         self.frnn = frnn
         self.brnn = brnn
+
     def transduce(self, embs, extras=None):
         fs = self.frnn.initial_state()
         bs = self.brnn.initial_state()
-        fs_states = fs.add_inputs(embs)   # 1, 2, 3, 4
+        fs_states = fs.add_inputs(embs)  # 1, 2, 3, 4
         bs_states = reversed(bs.add_inputs(reversed(embs)))  # 1, 2, 3, 4
         self.s = list(reversed(list(zip(fs_states, bs_states, extras))))  # 4, 3, 2, 1
         # special treatment for the final element
@@ -69,6 +80,7 @@ class Encoder(object):
         self.final_embedding = dy.concatenate([final_s[0].output(),
                                                final_s[1].output()])
         self.final_extra = final_s[2]
+
     def embedding(self, extra=False):
         if len(self.s) > 1:
             fs, bs, e = self.s[-1]
@@ -79,10 +91,13 @@ class Encoder(object):
         if extra:
             output = output, e
         return output
+
     def pop(self):
-        return self.s.pop()[-1] # return "extra" (i.e., whatever the caller wants or None)
+        return self.s.pop()[-1]  # return "extra" (i.e., whatever the caller wants or None)
+
     def __len__(self):
         return len(self.s)
+
     def copy(self):
         encoder = Encoder(self.frnn, self.brnn)
         encoder.s = list(self.s)  # copy
