2b3dbcca6df7aa83198e36de1f19f2e9fbc02420
davidgu13 <davidgu1312@gmail.com>
2022-09-03 12:51:38 +0300

    fixed validation and extraction of the results

--- extract_results.py
+++ extract_results.py
@@ -18,7 +18,7 @@ def find_2_floats_in_line(line):
 def extract_accuracies_from_single_folder(folder, io_format):
     # The folder has the format: Outputs_YYYY-MM-DD hhmmss_lang_pos_training-mode_IO_analogy_seed.
     # If the output format is not graphemes ('g'), then reevaluation was made at graphemes level.
-    assert io_format in {'g_g', 'f_f'}
+    assert io_format in {'g_g', 'f_f', 'f_p_attn'}
     datetime, lang, pos, training_mode = basename(folder).split("_")[1:5]
 
     lines = open(join(folder, 'f.stats'), encoding='utf8').read().split('\n')
@@ -27,8 +27,8 @@ def extract_accuracies_from_single_folder(folder, io_format):
     if io_format == 'g_g':
         dev_accuracy, test_accuracy, test_ed = find_float_in_line(lines[-3]), find_float_in_line(lines[-2]), -1
         test_graphemes_accuracy, test_graphemes_ed = None, None
-    elif io_format == 'f_f':
-        # See write_generalized_measures in utils.py; dev_ and test_ accuracy mean here eval at features level
+    elif io_format in {'f_f', 'f_p_attn'}:
+        # See util.write_generalized_measures; dev_ and test_ accuracy mean here eval at features level
         dev_accuracy, test_accuracy = find_float_in_line(lines[-6]), find_float_in_line(lines[-5])
 
         test_features_accuracy, test_ed = find_2_floats_in_line(lines[-3])
@@ -40,8 +40,8 @@ def extract_accuracies_from_single_folder(folder, io_format):
 
 
 def main():
-    results_folder = join('.', 'Results')
-    io_format = 'f_f' # 'g_g'
+    results_folder = join('.', 'Results', 'test_runs', 'f-p-attn')
+    io_format = 'f_p_attn' # 'g_g' or 'f_f'
     analogy_types, seeds  = ['None'], ['42'] # ['100', '42', '21']
 
     lang_pos_groups = dict(zip(range(1,16), ['kat_V', 'kat_N', 'fin_ADJ', 'swc_V', 'swc_ADJ', 'fin_V', 'sqi_V', 'hun_V',
@@ -50,7 +50,7 @@ def main():
 
     accuracies = []
     for analogy, seed in product(analogy_types, seeds):
-        current_parent_folder = join(results_folder, f"{analogy}_{seed}_{io_format}")
+        current_parent_folder = join(results_folder)  # , f"{analogy}_{seed}_{io_format}")
         assert isdir(current_parent_folder), f"Folder Results doesn't exist!"
 
         folders = [join(current_parent_folder, f) for f in os.listdir(current_parent_folder)
