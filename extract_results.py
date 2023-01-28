9dc0c99b1678b138c43e76d9203302d11982f013
davidgu13 <davidgu1312@gmail.com>
2023-01-28 18:47:47 +0200

    updated paths, configs & results extraction, added runs scripts

--- extract_results.py
+++ extract_results.py
@@ -1,12 +1,12 @@
 # This is a Python3 script for extracting the runs results to an Excel file, including final dev and test accuracies.
+import re
 from itertools import product
 from os import listdir
 from os.path import basename, isdir, join
-import re
-
 from pandas import DataFrame
-
 from util import are_substrings_in_string
+
+CUTOFF = len("Outputs_2023-01-28 135937_")
 
 def find_float_in_line(line):
     matches = re.findall("\d+\.\d+", line)
@@ -20,16 +20,14 @@ def find_2_floats_in_line(line):
 def extract_accuracies_from_single_folder(folder, io_format):
     # The folder has the format: Outputs_YYYY-MM-DD hhmmss_lang_pos_training-mode_IO_analogy_seed.
     # If the output format is not graphemes ('g'), then reevaluation was made at graphemes level.
-    assert io_format in {'g_g', 'f_f', 'f_p_attn'}
-    datetime, lang, pos, training_mode = basename(folder).split("_")[1:5]
 
     lines = open(join(folder, 'f.stats'), encoding='utf8').read().split('\n')
 
     test_ed, test_graphemes_accuracy, test_graphemes_ed = [-1] * 3
-    if io_format == 'g_g':
+    if io_format == 'gg':
         dev_accuracy, test_accuracy = find_float_in_line(lines[-3]), find_float_in_line(lines[-2])
 
-    else:  # io_format in {'f_f', 'f_p_attn'}
+    else:  # io_format in {'ff', 'fp'}
         # See util.write_generalized_measures; dev_ and test_ accuracy mean here eval at features level
         dev_accuracy, test_accuracy = find_float_in_line(lines[-6]), find_float_in_line(lines[-5])
 
@@ -38,33 +36,39 @@ def extract_accuracies_from_single_folder(folder, io_format):
 
         test_graphemes_accuracy, test_graphemes_ed = find_2_floats_in_line(lines[-2])
 
-    return datetime, lang, pos, training_mode, dev_accuracy, test_accuracy, test_ed, test_graphemes_accuracy, test_graphemes_ed
+    return dev_accuracy, test_accuracy, test_ed, test_graphemes_accuracy, test_graphemes_ed
 
 
 def main():
-    results_folder = join('.', 'Results')
-    io_format = 'f_p_attn'  # 'g_g' or 'f_f'
-    analogy_types, seeds = ['None'], ['100', '42', '21'] # ['42']
+    results_folder = join('.', 'Results', 'LearningCurves')
+    io_formats = ['gg', 'ff', 'fp']
+    analogy_types, seeds = ['None'], ['100', '42', '21']
+    train_samples = ['1000', '2000', '3000', '4000', '5000', '6000', '7000']
 
-    excel_results_file = f"Test-Results {'_'.join(analogy_types)}-{'_'.join(seeds)}-{io_format}.xlsx"
+    excel_results_file = f"Test-Results {'_'.join(analogy_types)}-{'_'.join(seeds)}-{'_'.join(io_formats)}-1k-7k.xlsx"
 
     run_records = []
-    for analogy, seed in product(analogy_types, seeds):
-        parent_folder = join(results_folder, f"{analogy}_{seed}_{io_format}")
-        assert isdir(parent_folder), f"Folder Results doesn't exist!"
+    for seed in seeds:
+        parent_folder = join(results_folder, f"{seed}")  # , f"{analogy}_{seed}_{io_format}")
+        assert isdir(parent_folder), f"Folder {parent_folder} doesn't exist!"
 
-        folders = [join(parent_folder, f) for f in listdir(parent_folder)
-                   if isdir(join(parent_folder, f)) and are_substrings_in_string(f[26:], (analogy, seed, io_format))]
+        folders = []
+        for f in listdir(parent_folder):
+
+            for current_combination in product(analogy_types, io_formats, train_samples):
+                if isdir(join(parent_folder, f)) and are_substrings_in_string(f[CUTOFF:], current_combination):
+                    folders.append(join(parent_folder, f))
 
         for folder in folders:
-            datetime, lang, pos, training_mode, dev_accuracy, test_accuracy, test_ed, \
-            test_graphemes_accuracy, test_graphemes_ed = extract_accuracies_from_single_folder(folder, io_format)
-            run_records.append([datetime, analogy, seed, lang, pos, training_mode, io_format, dev_accuracy,
-                                test_accuracy, test_ed, test_graphemes_accuracy, test_graphemes_ed])
+            datetime, lang, pos, training_mode, io_format, analogy, _, samples_number = basename(folder).split("_")[1:]
+            metrics = extract_accuracies_from_single_folder(folder, io_format)
+
+            run_records.append([datetime, analogy, seed, lang, pos, training_mode,
+                                io_format, samples_number, *metrics])
 
     df = DataFrame(run_records, columns=['DateTime', 'AnalogyMode', 'Seed', 'Language', 'POS', 'Form/Lemma', 'IO mode',
-                                         'Dev Accuracy', 'Test Accuracy', 'Test ED', 'Test Graphemes Accuracy',
-                                         'Test Graphemes ED'])
+                                         '#Train Samples', 'Dev Accuracy', 'Test Accuracy', 'Test ED',
+                                         'Test Graphemes Accuracy', 'Test Graphemes ED'])
     df = df.fillna("")
     df.to_excel(excel_results_file)
 
